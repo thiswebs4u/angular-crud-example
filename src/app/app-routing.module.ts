f9d664cab4a593cc6c728ecfa5b43ed0205cacdf
thiswebs4u <thiswebs4u@msn.com>
2020-07-29 16:54:04 -0600

    node modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './crud/home/home.component';
-import { DetailsComponent } from './crud/details/details.component';
-import { CreateComponent } from './crud/create/create.component';
-import { UpdateComponent } from './crud/update/update.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'crud/home', component: HomeComponent },
-  { path: 'crud/details/:productId', component: DetailsComponent },
-  { path: 'crud/create', component: CreateComponent },
-  { path: 'crud/update/:productId', component: UpdateComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,16 @@
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-
-import { CrudRoutingModule } from './crud/crud-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { CrudModule } from './crud/crud.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 
-
-// import { DetailsComponent } from './crud/details/details.component';
-// import { CreateComponent } from './crud/create/create.component';
-// import { UpdateComponent } from './crud/update/update.component';
-
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    CrudModule,
     BrowserModule,
-    FormsModule,
-    CommonModule,
-    CrudRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
