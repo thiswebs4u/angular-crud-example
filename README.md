d0c569fade0325fc3b4d71ced261d10e37851680
thiswebs4u <thiswebs4u@msn.com>
2020-07-30 07:42:34 -0600

    changed package.json

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AngularCrudExample
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.4.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CrudRoutingModule } from './crud/crud-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { CrudModule } from './crud/crud.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// import { DetailsComponent } from './crud/details/details.component';
+// import { CreateComponent } from './crud/create/create.component';
+// import { UpdateComponent } from './crud/update/update.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CrudModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    CommonModule,
+    CrudRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
